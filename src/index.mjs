8c7691784f9ffabdf6f920419c49ff94c9b61b60
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2023-12-10 07:20:16 +0100

    gallery

--- src/index.mjs
+++ src/index.mjs
@@ -1,23 +1,52 @@
 import axios from 'axios';
-import { Notify } from 'notiflix';
+import Notify from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 const form = document.querySelector('#search-form');
 const input = document.querySelector('input[name="searchQuery"]');
 const placeForImg = document.querySelector('.gallery');
-const search = document.querySelector('button');
+const moreBtn = document.querySelector('.load-more');
 const apiKey = '41167232-e4ed0bcecad469809d9012c23';
 let currentPage = 1;
 let searchingValue = '';
+let pageLimit = 5;
 function searchImages(query, page = 1) {
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${pageLimit}`;
   return axios
     .get(apiUrl)
     .then(res => {
       const data = res.data;
       if (data.hits && data.hits.length > 0) {
-        const html = data.hits.map(image => {
-          return `<img width="600" height="400" src="${image.largeImageURL}" class="cat-img"></img><h2 class="cat-name">${image.largeImageURL}</h2><p class="description">${image}</p>`;
-        });
-        placeForImg.innerHTML = html;
+        const html = data.hits.map(
+          ({
+            largeImageURL,
+            webformatURL,
+            tags,
+            likes,
+            views,
+            comments,
+            downloads,
+          }) => {
+            return `<div class="photo-card"><a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes: ${likes}</b>
+            </p>
+            <p class="info-item">
+              <b>Views: ${views}</b>
+            </p>
+            <p class="info-item">
+              <b>Comments: ${comments}</b>
+            </p>
+            <p class="info-item">
+              <b>Downloads: ${downloads}</b>
+            </p>
+          </div>
+          </a>
+        </div>`;
+          }
+        );
+        placeForImg.innerHTML = html.join('');
       } else {
         Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
@@ -31,8 +60,19 @@ function searchImages(query, page = 1) {
 }
 
 form.addEventListener('submit', ev => {
+  placeForImg.innerHTML = '';
   ev.preventDefault();
   searchingValue = input.value;
   currentPage = 1;
   searchImages(searchingValue, currentPage);
+  moreBtn.style.display = 'block';
+});
+moreBtn.addEventListener('click', () => {
+  currentPage++;
+  searchImages(searchingValue, currentPage);
+});
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
