3fc3b326e32d0941d73c1efbae4bc11396c8ff29
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2023-12-10 07:43:44 +0000

    Deploying to gh-pages from @ WKotlinski/goit-js-hw-11@4e44212d2f100fa1a5c873f6c0311d0f70534b75 🚀

--- index.303f11e0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-form"),t=document.querySelector('input[name="searchQuery"]');document.querySelector("button");let o="";e.addEventListener("submit",(e=>{e.preventDefault(),o=t.value,console.log(o)}));
-//# sourceMappingURL=index.303f11e0.js.map
--- index.e6e02710.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#search-form"),t=document.querySelector('input[name="searchQuery"]'),n=(document.querySelector("button"),"");e.addEventListener("submit",(function(e){e.preventDefault(),n=t.value,console.log(n)}))}();
-//# sourceMappingURL=index.e6e02710.js.map
